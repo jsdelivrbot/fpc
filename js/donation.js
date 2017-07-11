07267c047fcaa0457965dcbd6f5db372b8a7bcdf
Jonnathan Peche <jonnathan.pdiaz@gmail.com>
2017-07-11 17:46:25 -0500

    Ajustes

--- js/donation.js
+++ js/donation.js
@@ -7,29 +7,10 @@
 		template.id='donation-box';
 		template.innerHTML = '<div class="donation-hidden">'+
             '<a class="donation-close-widget" id="donation-close-widget">&times;</a>'+
-            '<div class="donation-form">'+
-                '<label class="donation-block align-left donation-text"><b>Tipo de Moneda</b>'+
-                '</label>'+
-                '<div class="donation-currency">'+
-                    '<button class="donation-inactive donation-button-default donate-currency">USD</button>'+
-                    '<button class="donation-inactive donation-button-default donate-currency">Soles</button>'+
-                '</div>'+
-                '<label class="donation-block align-left donation-text"><b>¿Cuánto deseas donar?</b>'+
-                '</label>'+
-                '<div class="amount">'+
-                    '<button class="button-45 donation-button-default donate-amount donation-inactive">20</button>'+
-                    '<button class="button-45 donation-button-default donate-amount donation-inactive">50</button>'+
-                    '<button class="button-45 donation-button-default donate-amount donation-inactive">100</button>'+
-                    '<button class="button-45 donation-button-default donate-amount donation-inactive">500</button>'+
-                '</div>'+
-                '<label class="donation-block align-left donation-text">Otras cantidades</label>'+
-                '<input type="text" id="customamount" class="donation-input" placeholder="Ej: 75">'+
-                '<button class="donation-button donation-button-default donation-active" id="donate">DONA AQUÍ</button>'+
-            '</div>'+
-            '<div class="donation" style="">'+
+            '<a href="https://www.joinnus.com/donacion/fpc" target="_blank" class="donation" style="">'+
                 '<div class="donation-block"><img class="donation-image" src="https://www.joinnus.com/inc/donation/images/img-latav2.png">'+
                 '</div><span class="donation-block donation-message">DONA AQUÍ</span><img class="powered_joinnus" src="https://www.joinnus.com/inc/donation/images/powered.png" alt="">'+
-            '</div>'+
+            '</a>'+
         '</div>'
 		document.body.appendChild(template);
 
